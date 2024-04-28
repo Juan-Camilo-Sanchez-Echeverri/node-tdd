b635f375195d02f69639f3dffd425f1eeb723b33
Juan Camilo Sanchez Echeverri <juancamilosanche65@gmail.com>
2024-04-28 17:19:19 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const { posts } = require('./endpoints');
+const { authenticate } = require('./middlewares');
+const services = require('./services')
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+const postsHandlers = posts(services);
+
+app.post('/', authenticate, postsHandlers.post);
+
+app.listen(port, () =>
+	console.log(`Example app listening at http://localhost:${port}`)
+)
+
+
+module.exports = app;
